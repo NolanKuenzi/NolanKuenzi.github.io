1e60715389d165d07a541cb044e1ecc10a40434a
Nolan Kuenzi <mr.nolank@gmail.com>
2019-04-01 00:16:00 -0700

    portfolio section update

--- src/portfolio.js
+++ src/portfolio.js
@@ -5,83 +5,85 @@ const Portfolio = () => (
     <div id="portfolioTitle">
       <h3>Portfolio:</h3>
     </div>
-    <div id="portfolioDescription">
-      <h4 id="portfolioDescriptionText">
-        Click on any of the thumbnail images to view the full project. Click on the GitHub icon
-        below to see the code.
-      </h4>
-      <a href="https://github.com/NolanKuenzi" id="gHub" target="_blank" rel="noopener noreferrer">
-        <img src="./dist/images/GH_icon.png" alt="" />
-        <h5 className="thumbnailText">GitHub</h5>
-      </a>
-    </div>
     <div className="portfolioSubText">
       <h4>Basic Projects:</h4>
     </div>
     <div className="row">
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/favoriteQuotes/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Random Quote Generator</h5>
           <img src="./dist/images/Favorite_Quotes.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Random Quote Generator</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/favoriteQuotes" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/favoriteQuotes/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/wikipediaViewer/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Wikipedia Viewer App</h5>
           <img src="./dist/images/Wikipedia_Viewer.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Wikipedia Viewer App</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/wikipediaViewer" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/wikipediaViewer/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
     </div>
     <div className="row">
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/basicCalculator/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">JavaScript Calculator</h5>
           <img src="./dist/images/JavaScript_Calculator.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">JavaScript Calculator</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/basicCalculator" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/basicCalculator/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/pomodoroTimer/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Pomodoro Timer</h5>
           <img src="./dist/images/Pomodoro_Timer.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Pomodoro Timer</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/pomodoroTimer" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/pomodoroTimer/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
     </div>
     <div className="row">
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/ticTacToeV2/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Tic-Tac-Toe Game</h5>
           <img src="./dist/images/Tic_Tac_Toe.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Tic-Tac-Toe Game</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/ticTacToeV2" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/ticTacToeV2/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/simonGame/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Simon Game</h5>
           <img src="./dist/images/Simon_Game.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Simon Game</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/simonGame" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/simonGame/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
     </div>
     <div className="portfolioSubText">
@@ -89,108 +91,132 @@ const Portfolio = () => (
     </div>
     <div className="row">
       <div className="imgDiv">
-        <a href="https://nolankuenzi.github.io/todoApp/" target="_blank" rel="noopener noreferrer">
+      <h5 className="thumbnailText">Todo-List App</h5>
           <img src="./dist/images/Todo_App.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Todo-List App</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/todoApp" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/todoApp/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/reactCalculator/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">React Calculator</h5>
           <img src="./dist/images/React_Calculator.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">React Calculator</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/reactCalculator" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/reactCalculator/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
     </div>
     <div className="row">
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/markdownPreviewer/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Markdown Previewer</h5>
           <img src="../dist/images/Markdown_Previewer.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Markdown Previewer</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/markdownPreviewer" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/markdownPreviewer/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/drumMachine/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Drum Machine</h5>
           <img src="./dist/images/Drum_Machine.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Drum Machine</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/drumMachine" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/drumMachine/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
     </div>
     <div className="row">
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/recipeBox/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Recipe Box App</h5>
           <img src="./dist/images/Recipe_Box.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Recipe Box App</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/recipeBox" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/recipeBox/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/localWeather/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Local Weather App</h5>
           <img src="./dist/images/Local_Weather.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Local Weather App</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/localWeather" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/localWeather/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
     </div>
     <div className="row">
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/pokemonHabitats/#/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Pokémon Habitats</h5>
           <img src="./dist/images/Pokemon_Habitats.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Pokémon Habitats</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/pokemonHabitats" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/pokemonHabitats/#/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/emojiSearch/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Emoji Search</h5>
           <img src="./dist/images/Emoji_Search.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Emoji Search</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/emojiSearch" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/emojiSearch/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
     </div>
     <div className="row">
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/topTwitchStreams/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Top Twitch Streams</h5>
           <img src="./dist/images/Top_Twitch_Streams.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Top Twitch Streams</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/topTwitchStreams" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/topTwitchStreams/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/TwitchClips/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Twitch Clips</h5>
           <img src="./dist/images/Twitch_Clips.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Twitch Clips</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/TwitchClips" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/TwitchClips/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
     </div>
     <div className="portfolioSubText">
@@ -198,46 +224,71 @@ const Portfolio = () => (
     </div>
     <div className="row">
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/d3BarChart/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Bar Chart</h5>
           <img src="./dist/images/Bar_Chart.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Bar Chart</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/d3BarChart" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/d3BarChart/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/d3Scatterplot/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Scatterplot Graph</h5>
           <img src="./dist/images/Scatterplot_Graph.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Scatterplot Graph</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/d3Scatterplot" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/d3Scatterplot/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
     </div>
     <div className="row">
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/d3HeatMap/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Heat Map</h5>
           <img src="./dist/images/Heat_Map.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Heat Map</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/d3HeatMap" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/d3HeatMap/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
       <div className="imgDiv">
-        <a
-          href="https://nolankuenzi.github.io/d3ChoroplethMap/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h5 className="thumbnailText">Choropleth Map</h5>
           <img src="./dist/images/Choropleth_Map.png" className="thumbnailImg" alt="" />
-          <h5 className="thumbnailText">Choropleth Map</h5>
-        </a>
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/d3ChoroplethMap" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://nolankuenzi.github.io/d3ChoroplethMap/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
+      </div>
+    </div>
+    <div className="portfolioSubText">
+      <h4>Full Stack Projects:</h4>
+    </div>
+    <div className="row">
+      <div className="imgDiv">
+      <h5 className="thumbnailText">Timestamp Microservice</h5>
+          <img src="./dist/images/timeStamp.png" className="thumbnailImg" alt="" />
+          <div>
+            <span className="thumbnailText">
+              <a href="https://github.com/NolanKuenzi/TimestampMicroservice" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </span>
+            <span className="thumbnailText">
+              <a href="https://rocky-taiga-66599.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Project</a>
+            </span>
+          </div>
       </div>
     </div>
   </div>
