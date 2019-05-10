4476c487e2b4de971921e07a00facb755438ce0a
Nolan Kuenzi <mr.nolank@gmail.com>
2019-05-10 01:10:14 -0700

    contact section update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactRouter from './js/router';
 import './myStyles.scss';
-import Main from './main';
 
 const App = () => (
   <div>
-    <Main />
+    <ReactRouter />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById('app'));
